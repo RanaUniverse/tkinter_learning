75b65d295a6f2dff62e904c87c786499098433ff
RanaUniverse <RanaUniverse321@gmail.com>
2025-04-24 14:27:19 +0530

    added a text on the windows with colors also

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import Tk, Label
 
 root = Tk()
 
@@ -7,5 +7,21 @@ width = 700
 height = 300
 size = f"{width}x{height}"
 root.geometry(size)
-root.configure(background="#FFFFFF")
+
+bg_color = "#FFFFFF"
+root.configure(background=bg_color)
+
+labeled_text = "Rana Universe"
+label = Label(
+    root,
+    text=labeled_text,
+    bg="#FFFFFF",
+    fg="green",
+    font=("Helvetica", 30, "bold"),
+    padx=10,
+    pady=10,
+)
+
+# label.pack()
+label.pack(side='left', fill='y', expand=True, padx=10, pady=10)
 root.mainloop()
